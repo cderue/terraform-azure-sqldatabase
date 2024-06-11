5a88a1d56bc5e713ce155c0139627293c82f26fc
Cédric Derue <cedric.derue@gmail.com>
2024-06-11 08:48:56 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,18 +13,15 @@ variable "resource_group_name_prefix" {
 variable "sql_db_name" {
   type        = string
   description = "The name of the SQL Database."
-  default     = "SampleDB"
 }
 
 variable "admin_username" {
   type        = string
   description = "The administrator username of the SQL logical server."
-  default     = "azureadmin"
 }
 
 variable "admin_password" {
   type        = string
   description = "The administrator password of the SQL logical server."
   sensitive   = true
-  default     = null
 }
