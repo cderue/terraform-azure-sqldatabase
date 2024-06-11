1112eb5593ac328d667a8a80e4d9045894bede80
Cédric Derue <cedric.derue@gmail.com>
2024-06-11 10:51:17 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -13,8 +13,4 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var_subscription_id
 }
